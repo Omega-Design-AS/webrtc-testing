e7cf0349ebbf31d9dc7a17f381ac33a287d2835a
Jostein Skaar <josteinsk@omega.no>
2021-03-02 14:14:13 +0100

    moved to https

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 var os = require('os');
 var nodeStatic = require('node-static');
-var http = require('http');
+var https = require('https');
 var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
+var app = https.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
 
